b2eafa88a8008cd1310b9cfa11a253acfad1f391
yoginth.eth <yoginth@hey.com>
2022-06-05 01:00:16 +0530

    chore: code splitup

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,18 +1,20 @@
 import ExploreFeed from '@components/Explore/Feed'
 import { GridItemEight, GridItemFour, GridLayout } from '@components/GridLayout'
-import Announcement from '@components/Home/Announcement'
 import Footer from '@components/Shared/Footer'
 import AppContext from '@components/utils/AppContext'
 import SEO from '@components/utils/SEO'
 import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
 import React, { useContext } from 'react'
 
 import HomeFeed from './Feed'
 import Hero from './Hero'
 import RecommendedProfiles from './RecommendedProfiles'
-import SetDefaultProfile from './SetDefaultProfile'
-import SetProfile from './SetProfile'
-import Streak from './Streak'
+
+const Announcement = dynamic(() => import('./Announcement'))
+const Streak = dynamic(() => import('./Streak'))
+const SetProfile = dynamic(() => import('./SetProfile'))
+const SetDefaultProfile = dynamic(() => import('./SetDefaultProfile'))
 
 const Home: NextPage = () => {
   const { currentUser } = useContext(AppContext)
